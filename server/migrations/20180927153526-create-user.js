48591c61e95c240cd41246973c88e1e03f7754a4
Nelson Nnaji <nnaji_udochukwu@yahoo.com>
2018-10-02 17:35:29 +0100

    modify the user migrations and models to accept username

--- server/migrations/20180927153526-create-user.js
+++ server/migrations/20180927153526-create-user.js
@@ -8,10 +8,18 @@ module.exports = {
         primaryKey: true,
         type: Sequelize.INTEGER
       },
+      username: {
+        allowNull: false,
+        unique: true,
+        type: Sequelize.STRING
+      },
       email: {
+        allowNull: false,
+        unique: true,
         type: Sequelize.STRING
       },
       password: {
+        allowNull: false,
         type: Sequelize.STRING
       },
       createdAt: {
--- server/models/user.js
+++ server/models/user.js
@@ -1,10 +1,10 @@
 export default (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
+    username: DataTypes.STRING,
     email: DataTypes.STRING,
     password: DataTypes.STRING
   }, {});
-  User.associate = models => {
-    // associations can be defined here
-  };
+  // User.associate = (models) => {
+  // };
   return User;
 };
