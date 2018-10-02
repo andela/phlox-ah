c86eba4bf5473fd360ae9c9c55ebfe6b55635e73
solomon <odumahs@gmail.com>
2018-10-02 16:14:21 +0100

    chore(coveralls): integrate coveralls
    - link coveralls with the repo
    - add a coveralls.yml file
    - add coveralls script to travis.yml file and package.json file
    - add script to create database in travis.yml file
    
    Finishes #160451494

--- server/index.js
+++ server/index.js
@@ -72,7 +72,7 @@ app.use((err, req, res, next) => {
 });
 
 // finally, let's start our server...
-const server = app.listen(process.env.PORT || 3000, () => {
+const server = app.listen(process.env.PORT || 5000, () => {
     console.log("Listening on port " + server.address().port);
 });
 
