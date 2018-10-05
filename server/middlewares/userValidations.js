7a9bc98ff392b422dbbda585569c3d0c5afd0585
Runor Adjekpiyede <kampkellykeys@gmail.com>
2018-10-05 12:57:23 +0100

    feature(validate-signup): implement feedback
    This commit implements the feedback gotten from the technical team lead.

--- server/middlewares/userValidations.js
+++ server/middlewares/userValidations.js
@@ -15,6 +15,8 @@ class UserValidations {
    * @returns {object} - status Message if validation fails or proceeds to next()
    */
   static validateSignup(req, res, next) {
+    // this function validates the input the user sends when trying to register
+    // this regex removes all the white space characters in the email and username
     req.body.email = req.body.email.toLowerCase().replace(/\s/g, '');
     req.body.username = req.body.username.replace(/\s/g, '');
     const joiSchema = {
@@ -22,14 +24,13 @@ class UserValidations {
       password: Joi.string().alphanum().min(8).required(),
       username: Joi.string().min(2).required(),
     };
+    // this tells Joi to proceed to check for all errors in user input before giving out a response
     const joiOptions = { abortEarly: false };
-    const {
-      username, email, password
-    } = req.body;
-    Joi.validate({
-      email, username, password,
-    }, joiSchema, joiOptions, (err) => {
+    const { username, email, password } = req.body;
+
+    Joi.validate({ email, username, password, }, joiSchema, joiOptions, (err) => {
       if (err) {
+        // this regex replaces the string (\") that is returned with the json response
         const getErrorMessages = error => error.message.replace(/"/g, '');
         res.status(422).json({ message: err.details.map(getErrorMessages), });
       } else if (!err) {
