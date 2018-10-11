244b3a9f2604dd9133c9892fcd289d23ab5bd192
solomon <odumahs@gmail.com>
2018-10-11 08:56:49 +0100

    feature(user-follow): author can follow/unfollow other authors    - add route that allows author follow other authors    - add route that allows author unfollow other authors    - add route that shows an authors followers and authors he follows
    Finishes #160451506

--- server/models/user.js
+++ server/models/user.js
@@ -1,6 +1,13 @@
 // add isVerified to the model file, it will be use to check if the user is verified.
 export default (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      unique: true,
+      primaryKey: true,
+    },
     username: {
       type: DataTypes.STRING,
       primaryKey: true,
@@ -66,6 +73,16 @@ export default (sequelize, DataTypes) => {
     User.hasMany(models.Article, {
       foreignKey: 'userId',
       as: 'articles'
+    });
+    User.belongsToMany(User, {
+      as: 'follower',
+      through: models.Followings,
+      foreignKey: 'follower',
+    });
+    User.belongsToMany(User, {
+      as: 'followed',
+      through: models.Followings,
+      foreignKey: 'followed',
     });
   };
   return User;
