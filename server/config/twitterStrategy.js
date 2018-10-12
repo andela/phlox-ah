554df7af590a63c430554b812c4693512781f51a
Nelson Nnaji <nnaji_udochukwu@yahoo.com>
2018-10-12 01:44:57 +0100

    install passport-twitter and setup twitter strategy

---
+++ server/config/twitterStrategy.js
@@ -0,0 +1,15 @@
+import { Strategy as TwitterStrategy } from 'passport-twitter';
+import socialLoginController from '../controllers/socialLoginController';
+
+// create an instance of Twitter Strategy
+const twitterStrategy = new TwitterStrategy(
+  {
+    consumerKey: process.env.TWITTER_API_KEY,
+    consumerSecret: process.env.TWITTER_API_SECRET,
+    callbackURL: `${process.env.BASE_URL}/login/twitter/return`,
+    includeEmail: true
+  },
+  socialLoginController.passportCallback
+);
+
+export default twitterStrategy;
