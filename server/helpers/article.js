3d4358bdd5b034f0c6e57ccf81a870f1eb47ddef
Runor Adjekpiyede <kampkellykeys@gmail.com>
2018-10-17 10:34:56 +0100

    feature(comments-history): disable eslint on isNaN
    This commit disables eslint checkinmg isNaN in articles helper.

--- server/helpers/article.js
+++ server/helpers/article.js
@@ -1,7 +1,8 @@
 
 export const computeOffset = (req) => {
   const page = Object.keys(req.query).length ? parseInt(req.query.page, 10) : 1;
-  return ((page <= 0) || (Number.isNaN(page))) ? 1 : page;
+  // eslint-disable-next-line
+  return ((page <= 0) || (isNaN(page))) ? 1 : page;
 };
 
 export const computeTotalPages = (data, limit) => Math.ceil(data.length / limit);
